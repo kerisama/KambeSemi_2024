69d7ec6392ceaa07fb9088d5d3a9bfa5f0373a1b
yura <yduara2248@gmail.com>
2024-11-18 17:43:17 +0900

    Sound Test GPIO

--- Sound_test.py
+++ Sound_test.py
@@ -1,3 +1,19 @@
-import subprocess
 
-subprocess.Popen(['mpg321', 'sample1.mp3'])
+#coding:utf-8
+
+import RPi.GPIO as GPIO
+import time
+
+pin = 21
+
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(pin,GPIO.OUT,initial=GPIO.LOW)
+
+p = GPIO.PWM(pin,1)
+p.start(50)
+
+p.ChangeFrequency(220)
+time.sleep(3)
+
+p.stop()
+GPIO.cleanup()
