6f2b3be61856015eba5c6a81c9812eb0eb6c6eee
yura <yduara2248@gmail.com>
2024-11-25 17:59:31 +0900

    loop code

--- Sound_test_loop.py
+++ Sound_test_loop.py
@@ -1,4 +1,17 @@
 import subprocess
+import time
 
-while True:
-    subprocess.Popen(['mpg321', 'sample1.mp3'])
+heavy = 30
+music = None
+
+if 10 < heavy < 50:
+    music = 'sample1.mp3'
+elif 50 <= heavy < 150:
+    music = 'sample2.mp3'
+else:
+    music = None
+
+if music:
+    while heavy > 10:
+        process = subprocess.Popen(['mpg321', music])
+
---
+++ Sound_test_mp3.py
@@ -0,0 +1,4 @@
+import subprocess
+
+while True:
+    subprocess.Popen(['mpg321', 'sample1.mp3'])
